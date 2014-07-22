34e7d461cdf43b2e7db10e895a5ed53072133919
Jules DELARUE <jules.delarue@worldline.com>
2014-07-22 18:13:19 +0200

    fix hiwr_msg

--- include/hiwr_simple_example.h
+++ include/hiwr_simple_example.h
@@ -1,49 +1,78 @@
+/*********************************************************************
+*
+*
+* Copyright 2014 Worldline
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+***********************************************************************/
+
+//Common
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-// Touchscreen export
-#include "ros/ros.h"
-#include "rospack/rospack.h"
-#include "hyve_msg/TouchEvent.h"
-
-// Facetracking export
-#include <dlfcn.h>
-#include <unistd.h>
-#include <signal.h>
 #include <cstdio>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
+//#include <dlfcn.h>
+#include <unistd.h>
+
+// ROS
+#include "ros/ros.h"
 #include <ros/time.h>
+#include "rospack/rospack.h"
+
+// Messages
+#include "sensor_msgs/RegionOfInterest.h"
 #include "std_msgs/String.h"
 #include "std_msgs/UInt8.h"
-#include <hyve_msg/SetState.h>
-#include <hyve_msg/GetState.h>
-#include <std_msgs/String.h>
-#include "sensor_msgs/RegionOfInterest.h"
-#include <string.h>
+
+// Service Msgs
+#include <hiwr_msg/SetState.h>
+#include <hiwr_msg/GetState.h>
+
+// TouchScreen Msgs
+#include "hiwr_msg/TouchEvent.h"
 
 
 #include <pluginlib/class_list_macros.h>
 
 class HiwrSimpleExample {
 public:
-    void callbackTS(const hyve_msg::TouchEvent::ConstPtr & touch);
-    void callbackROI(const sensor_msgs::RegionOfInterest::ConstPtr &msg);
-    void callbackTired(const std_msgs::UInt8::ConstPtr &);
+    HiwrSimpleExample(ros::NodeHandle &);
     void loop();
 
-    HiwrSimpleExample(ros::NodeHandle &);
-    ros::NodeHandle nh_;
-    bool pressed_;
-    std_msgs::String scenario_state_;
-    ros::Publisher pub_scenario_;
-    ros::Subscriber sub_tired_;
-    bool is_sleeping_;
-    int last_brightness_;
 protected:
 
 private:
+    ros::NodeHandle nh_;
+
+    std_msgs::String scenario_state_;
+
+    // Publishers
+    ros::Publisher pub_scenario_;
+
+    //Subscribers
+    ros::Subscriber sub_tired_;
+    ros::Subscriber sub_ts_;
+
+    // Hiwr states
+    bool is_sleeping_;
+    bool pressed_;
+    bool send_sleep_;
+    bool send_wakeup_;
+
+    int last_brightness_;
+
+    void callbackTS(const hiwr_msg::TouchEvent::ConstPtr & touch);
+    void callbackTired(const std_msgs::UInt8::ConstPtr &);
 
 };
--- src/hiwr_simple_example.cpp
+++ src/hiwr_simple_example.cpp
@@ -1,3 +1,22 @@
+/*********************************************************************
+*
+*
+* Copyright 2014 Worldline
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+***********************************************************************/
+
 #include "hiwr_simple_example.h"
 
 HiwrSimpleExample::HiwrSimpleExample(ros::NodeHandle &n):
@@ -5,90 +24,88 @@ HiwrSimpleExample::HiwrSimpleExample(ros::NodeHandle &n):
 {
 }
 
-void HiwrSimpleExample::callbackTS(const hyve_msg::TouchEvent::ConstPtr & touch) {
-    ROS_INFO("Entering callback TOUCH");
+void HiwrSimpleExample::callbackTS(const hiwr_msg::TouchEvent::ConstPtr & touch) {
+    ROS_DEBUG("Entering callback TOUCH");
 
     pressed_ = touch->pressed;
 }
 
-void HiwrSimpleExample::callbackROI(const sensor_msgs::RegionOfInterest::ConstPtr &msg) {
-    ROS_INFO("Catching someone's face");
-    scenario_state_.data = "8";
-    pub_scenario_.publish(scenario_state_);
-
-    ros::Duration(1).sleep();
-    scenario_state_.data = "6";
-    pub_scenario_.publish(scenario_state_);
-}
-
 void HiwrSimpleExample::callbackTired(const std_msgs::UInt8::ConstPtr &msg){
- // ROS_INFO("Getting new tired value: %d and bright = %d", msg->data, last_brightness);	
-//Retrieve first brightness value
+    //Retrieve first brightness value
   	last_brightness_ = msg->data;
   
 	//Mean last_value with new data
 	if((msg->data+last_brightness_)*0.5 < 25){
 		is_sleeping_ = true;
-    ROS_INFO("Going to sleep");
+        ROS_DEBUG("Going to sleep");
 	}
 	else is_sleeping_ = false;
 }
 
 void HiwrSimpleExample::loop(){
     ros::Rate loop_rate(10);
-    pressed_ = false;
-    ROS_INFO("Initializing subsriber");
-    ros::Subscriber sub_ts;
-    ros::Subscriber sub_roi;
-    is_sleeping_ = false;
-    last_brightness_ = -1;
-    sub_ts = nh_.subscribe("/hiwr_touchscreen/touch_event",1, &HiwrSimpleExample::callbackTS,this);
-    //sub_roi = nh.subscribe<sensor_msgs::RegionOfInterest>("/hiwr_opencv_detector/roi", 1, &HiwrSimpleExample::callbackROI, this);
-    pub_scenario_ = nh_.advertise<std_msgs::String>("/scenario/state", 1);
-    sub_tired_ = nh_.subscribe("/hiwr_tired/brightness", 1, &HiwrSimpleExample::callbackTired, this);
-    bool send_sleep = false;
-    bool send_wakeup = false;
-    while(ros::ok()) {
-        if(pressed_ && !is_sleeping_) {
 
-            ROS_INFO("Pressed");
-            scenario_state_.data = "7"; // grumpy face
+    pressed_ = false;
+    is_sleeping_ = false;
+    send_sleep_ = false;
+    send_wakeup_ = false;
+    last_brightness_ = -1;
+
+    ROS_DEBUG("Initializing subsriber");
+    sub_ts_ = nh_.subscribe("/hiwr_touchscreen/touch_event",1, &HiwrSimpleExample::callbackTS,this);
+    sub_tired_ = nh_.subscribe("/hiwr_tired/brightness", 1, &HiwrSimpleExample::callbackTired, this);
+
+    pub_scenario_ = nh_.advertise<std_msgs::String>("/scenario/state", 1);
+
+    while(ros::ok()) {
+        // Touchscreen pressed
+        if(pressed_ && !is_sleeping_) {
+            ROS_DEBUG("Pressed");
+
+            //Display grumpy face
+            scenario_state_.data = "7";
             pub_scenario_.publish(scenario_state_);
+
             ros::Duration(2).sleep();
+
+            //Back to normal head
             scenario_state_.data = "6";
             pub_scenario_.publish(scenario_state_);
-            //ros::Publisher pub = nh.advertise<hyve_msg::TouchEvent>("hyve_screen", sizeof(hyve_msg::TouchEvent));
 
-        }else if(is_sleeping_ && !send_sleep){
-		scenario_state_.data="3";
-		pub_scenario_.publish(scenario_state_);
-		ros::Duration(1).sleep();
-        scenario_state_.data="4";
-		pub_scenario_.publish(scenario_state_);
-		send_sleep = true;
-		send_wakeup = false;	
-	}else if(!is_sleeping_ && !send_wakeup){
-        scenario_state_.data="6";
-        pub_scenario_.publish(scenario_state_);
-		
-		send_sleep = false;
-		send_wakeup = true;
-	}
+        }else if(is_sleeping_ && !send_sleep_){
+            // Hiwr being tired
+            scenario_state_.data="3";
+            pub_scenario_.publish(scenario_state_);
+
+            ros::Duration(1).sleep();
+
+            // Hiwr sleepy
+            scenario_state_.data="4";
+            pub_scenario_.publish(scenario_state_);
+
+            send_sleep_ = true;
+            send_wakeup_ = false;
+        }else if(!is_sleeping_ && !send_wakeup_){
+            // Awake
+            scenario_state_.data="6";
+            pub_scenario_.publish(scenario_state_);
+
+            send_sleep_ = false;
+            send_wakeup_ = true;
+        }
+
         loop_rate.sleep();
         ros::spinOnce();
     }
 }
 
-
 int main(int argc, char ** argv) {
-    ROS_INFO("Initializing...");
+    ROS_DEBUG("Initializing...");
     ros::init(argc, argv, "HiwrSimpleExample");
     ros::NodeHandle n;
 
     HiwrSimpleExample node(n);
     node.loop();
-
-
 }
 
 
